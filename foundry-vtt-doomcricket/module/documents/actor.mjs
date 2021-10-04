bd1c5c6c310ae25e589e8413e0b8c1a5da6e9f89
William Bidstrup <williambidstrup@192.168.20.13>
2021-10-04 15:02:12 +1030

    chore: updated version number for v0.0.3

--- foundry-vtt-doomcricket/module/documents/actor.mjs
+++ foundry-vtt-doomcricket/module/documents/actor.mjs
@@ -2,7 +2,7 @@
  * Extend the base Actor document by defining a custom roll data structure which is ideal for the Simple system.
  * @extends {Actor}
  */
-export class DoomCricketActorextends Actor {
+export class DoomCricketActor extends Actor {
 
   /** @override */
   prepareData() {
