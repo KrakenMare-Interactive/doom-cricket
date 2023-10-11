bee4cea27462071075fb1ab2c2882be7584108cf
William Bidstrup <williambidstrup@192.168.20.17>
2023-10-12 08:04:55 +1030

    feat: create data_types program script

---
+++ data_types.c
@@ -0,0 +1,16 @@
+/* Define some data types */
+
+int age = 30; // integer type
+char firstLetter = 'D' // character type
+double pi = 3.14159265358979323846; // a double-precision floating point
+
+printf("Age: %d\n", age);
+printf("First letter: %c\n", firstLetter);
+printf("Value of pi: %lf\n", pi);
+
+/* Program to get user input */
+
+char dish[50]; // this is an array of characters, one is reserved
+printf("What's your favourite spicy food? \n");
+scanf("%s", dish); // user input must be less than 49 chararacters
+printf("Ah, the amazing spicy %s! A dish worthy of a jungle feast! \n", dish);
