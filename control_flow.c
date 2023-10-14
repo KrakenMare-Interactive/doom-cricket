055443dd82d80e7d4e97679c656fa93464fb5c6c
William Bidstrup <williambidstrup@192.168.20.17>
2023-10-15 09:34:20 +1030

    feat: finish control_flow game, it works

--- control_flow.c
+++ control_flow.c
@@ -5,14 +5,14 @@ To compile, run gcc -o control_flow control_flow.c */
 
 int main() {
 
-  // Prompt user to play the game and define their choice.
+  // Prompt user to play the game and define the `choice`.
   int choice;
   printf("Bless up! In dis game yuh gon seleck a number and I will return a spicy"
         "flavour for yuh! \n"
         "Now, please preach a number between 1 and 3, nuff said:  \n");
   scanf("%d", &choice);
 
-  // Define the spice level based on user input.
+  // Define the `spiceLevel` based on user input.
   int spiceLevel;
   switch (choice) {
   case 1:
@@ -29,6 +29,14 @@ int main() {
       return 1;
   }
 
+  // Suggest a spicy dish based on the `spiceLevel`
+if (spiceLevel == 1) {
+    printf("Mi recommend di Mild Jerk Chicken. \n");
+} else if (spiceLevel == 2) {
+    printf("Try di Spicy Cassowary Gumbo mon!. \n");
+} else if (spiceLevel == 3) {
+    printf("If yuh brave, try di Warlock Inferno Sauce! \n");
+}
 
 
 }
