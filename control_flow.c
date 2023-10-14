e3c416ebfb40c7e82605505449115ce2dd54f459
William Bidstrup <williambidstrup@192.168.20.17>
2023-10-15 09:22:05 +1030

    feat: control_flow game, define spice level.

--- control_flow.c
+++ control_flow.c
@@ -1,4 +1,5 @@
-/* This program asks a user to choose a number for a spicy dish. */
+/* This program asks a user to choose a number for a spicy dish.
+To compile, run gcc -o control_flow control_flow.c */
 
 #include <stdio.h>
 
@@ -8,8 +9,26 @@ int main() {
   int choice;
   printf("Bless up! In dis game yuh gon seleck a number and I will return a spicy"
         "flavour for yuh! \n"
-        "Now, please select a number between 1 and 3, nuff said:  \n");
+        "Now, please preach a number between 1 and 3, nuff said:  \n");
   scanf("%d", &choice);
+
+  // Define the spice level based on user input.
+  int spiceLevel;
+  switch (choice) {
+  case 1:
+      spiceLevel = 1;
+      break;
+  case 2:
+      spiceLevel = 2;
+      break;
+  case 3:
+      spiceLevel = 3;
+      break;
+  default:
+      printf("That choice nah de pon the list bredren. \n");
+      return 1;
+  }
+
 
 
 }
