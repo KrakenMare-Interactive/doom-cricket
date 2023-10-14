0ab4e7290d4fe4445b42cd9e2a04fc83bf98ea62
William Bidstrup <williambidstrup@192.168.20.17>
2023-10-15 09:12:14 +1030

    feat: control_flow game, prompt for user input.

---
+++ control_flow.c
@@ -0,0 +1,15 @@
+/* This program asks a user to choose a number for a spicy dish. */
+
+#include <stdio.h>
+
+int main() {
+
+  // Prompt user to play the game and define their choice.
+  int choice;
+  printf("Bless up! In dis game yuh gon seleck a number and I will return a spicy"
+        "flavour for yuh! \n"
+        "Now, please select a number between 1 and 3, nuff said:  \n");
+  scanf("%d", &choice);
+
+
+}
